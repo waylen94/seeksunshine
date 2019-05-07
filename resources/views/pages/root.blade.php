1553168ef629a03b14d7b2774a713477adbf88e6
Weilun Liu <763027562@qq.com>
2019-05-07 04:12:15 +0000

    adjusting the homepage

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -2,5 +2,21 @@
 @section('title', 'homepage')
 
 @section('content')
-  <h1>this is homepage</h1>
+      <div class="masthead">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+     @include('shared._messages')
+        <div class="col-lg-10 align-self-end">
+          <h1 class="text-uppercase text-white font-weight-bold">Seek Your Sunshine</h1>
+      </div>
+    </div>	
+		<div class="row sonar-wrapper">
+		<button type ="button" class="sonar-emitter text-center text-white mt-0 btn-lg orange-circle-button btn btn-primary " onclick="window.location.href = '{{ route('events.index') }}';" > Seek <br />your <br />Sunshine
+		<div class="sonar-wave"></div>
+		</button>
+		
+		
+		</div>
+        </div>
+        
+
 @stop
